2e6ea37924f14cbf165dbb925c0c554a36a756e3
amitverma2699 <amit99bhama@gmail.com>
2025-01-03 21:33:17 +0530

    Add files

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+
+package_name="DiamondsPricePrediction"
+
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/pipelines/__init__.py",
+    "src/pipelines/training_pipeline.py",
+    "src/pipelines/prediction_pipeline.py",
+    "src/logger.py",
+    "src/exception.py",
+    "src/utils/__init__.py",
+    "notebooks/research.ipynb",
+    "notebooks/data/.gitkeep",
+    "requirements.txt",
+    "setup.py",
+    "init_setup.sh",
+]
+
+
+# create a directory
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+    else:
+        print("file already exists")
+
