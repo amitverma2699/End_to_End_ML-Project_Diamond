a9e07900bf3204f60b91a3ac1939cb6825c4daf0
amitverma2699 <amit99bhama@gmail.com>
2025-01-09 14:53:42 +0530

    Update File

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from src.pipelines.prediction_pipeline import CustomData
+from src.pipelines.prediction_pipeline import PredictPipeline, CustomData
+from flask import Flask, request, jsonify,render_template
 
-obj=CustomData(0.7,61.2,57.0,5.69,5.73,3.5,'Ideal','G','VS1')
-print(obj.get_data_as_dataframe())
+app=Flask(__name__)
 
-#[0.7,61.2,57.0,5.69,5.73,3.5,'Ideal','G','VS1']
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["GET","POST"])
+def predict_datapoint():
+    if request.method=="GET":
+        return render_template("form.html")
+    else:
+        data=CustomData(
+            
+            carat=float(request.form.get("carat")),
+            depth=float(request.form.get("depth")),
+            table=float(request.form.get("table")),
+            x=float(request.form.get("x")),
+            y=float(request.form.get("y")),
+            z=float(request.form.get("z")),
+            cut=request.form.get("cut"),
+            color=request.form.get("color"),
+            clarity=request.form.get("clarity")
+        )
+        
+        final_data=data.get_data_as_dataframe()
+        prediction=PredictPipeline()
+        pred=prediction.predict(final_data)
+        result=round(pred[0],2)
+        return render_template("result.html",result=result)
+    
+
+if __name__ == "__main__":
+    app.run()
